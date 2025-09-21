443ffed5a7314ca21e11f2c1e84375214226b399
clara123-e <elikemclara@gmail.com>
2025-09-21 20:41:44 +0000

    added removeProduct function

--- Inventory.js
+++ Inventory.js
@@ -1,2 +1,9 @@
-the name it bit
-let val = []
+const removeProduct = (id) => {
+const index = inventory.findIndex ((item) => item.find === id);
+if (index === -1) {
+return "Product not found";
+}
+let removeIndex = inventory.splice (index, 1);
+return `${removeIndex[0].name} has been removed`;
+ 
+};
