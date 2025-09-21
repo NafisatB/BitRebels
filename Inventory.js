b2e6ff112ec97aeaeb737e235dfcb47dbaa625ef
Harobed1724 <deborahakoranteng@gmail.com>
2025-09-21 22:13:01 +0000

    Checker added

--- Inventory.js
+++ Inventory.js
@@ -1,1 +1,15 @@
-let Name = 3;
+console.log(inventory.length, "before adding");
+ 
+addProduct(1, "Laptop", 1200, 5);
+addProduct(2, "Phone", 600, 15);
+addProduct(3, "Milk", 700, 10);
+addProduct(4, "Charger", 950, 4);
+addProduct(5, "Tape", 60, 7);
+ 
+console.log(inventory.length, "after adding");
+console.log(inventory, "DATABASE");
+ 
+console.log(updateStock(1, 5));
+console.log(removeProduct(2));
+ 
+console.log(generateReport());
