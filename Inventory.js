3a460bf29d50415287e41767ed2c7be215194be0
Goziem <nneomaiheanachoelizabeth@gmail.com>
2025-09-21 21:23:21 +0100

    updated stock

--- Inventory.js
+++ Inventory.js
@@ -1,1 +1,10 @@
-let name = Elizabeth;
+// Update stock
+const updateStock = (id, change) => {
+  const product = findProductById(id);
+  if (!product) {
+    return `Product with ID ${id} not found.`;
+  }
+  product.quantity += change;
+  product.lowStock = product.quantity < 10;
+  return `Updated ${product.name}, new quantity: ${product.quantity}`;
+};
