d330da8c3242a38b2ee2470b2b9d1f3d1637149b
Nafisat <nafisatbabamusa@outlook.com>
2025-09-22 10:41:53 +0100

    final final final

--- Inventory.js
+++ Inventory.js
@@ -60,6 +60,9 @@ console.log(inventory.length, "before adding");
  
 addProduct(1, "Laptop", 1200, 5);
 addProduct(2, "Phone", 600, 15);
+addProduct(3, "Milk", 700, 10);
+addProduct(4, "Charger", 950, 4);
+addProduct(5, "Tape", 60, 7);
  
 console.log(inventory.length, "after adding");
 console.log(inventory, "DATABASE");
